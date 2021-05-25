314c3587a068cb00e1276f62d3dc62af77809ac8
awstah <62381031+awstah@users.noreply.github.com>
2021-05-25 01:12:41 -0700

    Header.js
    File is created and import to index.js

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div>
+      {/**logo */}
+      {/**menu */}
+      {/**login button */}
+    </div>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -7,6 +8,8 @@ export default function Home() {
         <title>Buzzinessware</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header />
 
       <main>
         <h2>Welcom to buzzinessware a project management tool</h2>
