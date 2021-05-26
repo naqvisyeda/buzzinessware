23086f91e16e8b3b3e0221975b38968d54dba58a
naqvisyeda <76452305+naqvisyeda@users.noreply.github.com>
2021-05-26 23:35:13 +0500

    auth.js designed
    auth.js design completed.

--- pages/auth.js
+++ pages/auth.js
@@ -5,15 +5,24 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getProviders, getSession, signIn } from "next-auth/client";
+import { urlObjectKeys } from "next/dist/next-server/lib/utils";
 
 function auth({ providers }) {
   console.log(providers);
   return (
     <div className="grid grid-cols-12">
-      <div className="col-span-3 p-10">
-        <h2>Login to your account</h2>
+      <div className="col-span-3 p-10 justify-center">
+        <div>
+          <h2 className="text-2xl font-extrabold text-black cursor-pointer ">
+            Buzzinessware
+          </h2>
+        </div>
 
-        <div className="flex flex-col space-y-4 items-center justify-center">
+        <h2 className="mt-40 mb-5 text-2xl font-bold ">
+          Log into your account
+        </h2>
+
+        <div className="flex flex-col space-y-7 mt-30 items-center justify-center">
           <button
             className="bw-login border-gray-400 from-gray-600 to-gray-500 hover:to-gray-700 "
             onClick={() => {
@@ -46,8 +55,11 @@ function auth({ providers }) {
         </div>
       </div>
 
-      <div className="col-span-9">
-        <h2>Hello</h2>
+      <div className="col-span-9 bg-gray-50">
+        <div
+          style={{ backgroundImage: `url("/Images/loginhero.svg")` }}
+          className="w-full h-screen bg-no-repeat	"
+        />
       </div>
     </div>
   );
